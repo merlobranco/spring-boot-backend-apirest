3e3cb1efc0ed9375a9dc14288e35a1eb0ddc8e71
Brais Cidras <braisomary@gmail.com>
2021-01-31 20:51:46 +0200

    Adding update and delete methods to Backend Controller Api Rest

--- src/main/java/com/merlobranco/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/merlobranco/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,6 +40,17 @@ public class ClienteRestController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente create(@RequestBody Cliente cliente) {
 		return clienteService.save(cliente);
+	}
+	
+	@PutMapping("/clientes")
+	public Cliente update(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
 	}
 
 }
