7cd33856ad23a4fe791cac797eb7dbd93f4883a8
Brais Cidras <braisomary@gmail.com>
2021-01-31 20:05:34 +0200

    Adding CRUD methods to the Backend ClienteServcie class

--- src/main/java/com/merlobranco/springboot/backend/apirest/models/service/ClienteService.java
+++ src/main/java/com/merlobranco/springboot/backend/apirest/models/service/ClienteService.java
@@ -7,5 +7,11 @@ import com.merlobranco.springboot.backend.apirest.models.entity.Cliente;
 public interface ClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
 
 }
